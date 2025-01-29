4377ecb9cd469c59c6e2989feef5e33116e26e25
eplxy <steven.dvlam@gmail.com>
2025-01-29 17:33:11 -0500

    Start setting up pages, basic routing

---
+++ app/frontend/src/components/RegisterForm.tsx
@@ -0,0 +1,60 @@
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+export interface IRegisterFormProps {}
+
+export default function RegisterForm(props: IRegisterFormProps) {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Username:", username, "Password:", password);
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          p: 3,
+          boxShadow: 3,
+          borderRadius: 2,
+          bgcolor: "background.paper",
+        }}
+      >
+        <Typography variant="h5" align="center" gutterBottom>
+          Register
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            label="Username"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            onChange={(e) => setUsername(e.target.value)}
+            autoComplete="current-username"
+          />
+          <TextField
+            label="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ mt: 2 }}
+          >
+            Register
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
+}
