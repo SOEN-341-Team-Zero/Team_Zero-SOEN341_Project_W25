5b64effd0daf6e530f096f2b2bf7f59dab4788df
“Mikachupichu” <michael.pouget01@gmail.com>
2025-01-30 19:37:12 -0500

    Set up backend-database integration

--- app/backend/Models/UserModel.cs
+++ app/backend/Models/UserModel.cs
@@ -11,13 +11,15 @@ namespace UserModel.Models
         Admin
         }
         [Required]
+        [Key] // Primary
+        private int id {get; set; }
+        [Required]
         [StringLength(25)] // Max username length
         public required string Username { get; set; }
         [Required]
         public required Roles Role { get; set; }
         [Required]
         [StringLength(254)] // Max email address length (set by RFC 5321 and SMTP constraints)
-        [Key]  // Primary
         public required string EmailAddress { get; set; }
         [Required]
         [StringLength(25)] // Max password length
