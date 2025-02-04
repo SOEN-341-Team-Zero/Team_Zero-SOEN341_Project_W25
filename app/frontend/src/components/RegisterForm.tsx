49c429f2c73507487f5c2bee4376b644f1a9cdab
“Mikachupichu” <michael.pouget01@gmail.com>
2025-02-04 12:42:01 -0500

    Created and set up the register controller and set up its backend-frontend endpoints

--- app/frontend/src/components/RegisterForm.tsx
+++ app/frontend/src/components/RegisterForm.tsx
@@ -1,15 +1,42 @@
 import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
-
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 export interface IRegisterFormProps {}
 
 export default function RegisterForm(props: IRegisterFormProps) {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const navigate = useNavigate();
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Username:", username, "Password:", password);
+    try {
+          const response = await fetch(`/api/register/validate`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: `${username}`,
+              password: `${password}`,
+            }),
+          });
+    
+          if (response.ok) {
+            const data = await response.json();
+            console.log("Register and login successful");
+            // this is the auth token for the API endpoints
+            localStorage.setItem("jwt-token", data.token);
+            //this cookie is only for rendering. API is authenticated using JWT.
+            Cookies.set("isLoggedIn", "true", { expires: 1, path: "/" }); //
+            navigate("/home");
+          } else {
+            console.error("Error:");
+          }
+        } catch (error) {
+          console.error("Network Error:", error);
+        }
   };
 
   return (
