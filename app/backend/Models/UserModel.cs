9201724e2ae54fe6d1d0c5cc87c134de9ae54742
meloniouss <andrei.jianu31@gmail.com>
2025-02-04 15:40:29 -0500

    Integrate front-end back-end user creation

--- app/backend/Models/UserModel.cs
+++ app/backend/Models/UserModel.cs
@@ -4,11 +4,6 @@ namespace ChatHaven.Models
 {
     public class User
     {
-        public enum Roles
-        {
-            Member,
-            Admin
-        }
 
         [Key] // Primary key (Optional, EF assumes Id as PK)
         public int user_id { get; set; }
@@ -18,14 +13,11 @@ namespace ChatHaven.Models
         public string username { get; set; } = string.Empty;
 
         [Required]
-        public string role { get; set; }
-
-        [Required]
-        [StringLength(254, MinimumLength = 5)] // Email constraints
-        public string email { get; set; } = string.Empty;
+        public Boolean isAdmin { get; set; }
 
         [Required]
         [StringLength(25, MinimumLength = 1)] // Password length
         public string password { get; set; } = string.Empty;
+
     }
 }
--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./components/HomePage";
 import RegisterPage from "./pages/RegisterPage";
 import { createTheme, ThemeProvider } from "@mui/material";
+import Cookies from "js-cookie";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const loggedIn = Cookies.get("isLoggedIn") === "true"; 
+    setIsAuthenticated(loggedIn);
+  }, []); 
+
   const theme = createTheme({
     palette: {
       mode: "dark",
@@ -19,11 +28,18 @@ function App() {
     <ThemeProvider theme={theme}>
       <Router>
         <Routes>
-          <Route path="/" element={<LoginPage />} />{" "}
-          {/* will need to redefine */}
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<HomePage />} /> {/* only if authed */}
-          {/* will need to redefine */}
+          <Route 
+            path="/" 
+            element={isAuthenticated ? <Navigate to="/home" /> : <LoginPage />} 
+          />
+          <Route 
+            path="/register" 
+            element={isAuthenticated ? <Navigate to="/home" /> : <RegisterPage />} 
+          />
+          <Route 
+            path="/home" 
+            element={isAuthenticated ? <HomePage /> : <Navigate to="/" />} 
+          />
         </Routes>
       </Router>
     </ThemeProvider>
