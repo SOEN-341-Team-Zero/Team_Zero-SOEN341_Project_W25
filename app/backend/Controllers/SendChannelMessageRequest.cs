cc55b527d51caeb6c5e30062de19b1d7cab80816
Mikachupichu <143844781+Mikachupichu@users.noreply.github.com>
2025-02-13 17:00:15 -0500

    Update SendChannelMessageRequest.cs

--- app/backend/Controllers/SendChannelMessageRequest.cs
+++ app/backend/Controllers/SendChannelMessageRequest.cs
@@ -1,6 +1,6 @@
 public class SendChannelMessageRequest
 {
     public required string Message { get; set; }
-    public required string SenderId { get; set; }
+    public required int SenderId { get; set; }
     public required int ChannelId {get;set;}
 }
