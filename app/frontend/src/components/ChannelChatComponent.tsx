600ca82ac55562f14c6549148b9684676584413c
meloniouss <andrei.jianu31@gmail.com>
2025-02-14 19:16:16 -0500

    Add message saving and fetching (#135)

--- app/frontend/src/components/ChannelChatComponent.tsx
+++ app/frontend/src/components/ChannelChatComponent.tsx
@@ -4,6 +4,7 @@ import { Box, Container, Grid2 as Grid, TextField } from "@mui/material";
 import { IChannelMessageModel } from "../models/models";
 import "../styles/ChatArea.css";
 import ChatMessage from "./ChatMessage";
+import wretch from "wretch";
 
 interface ChannelChatComponentProps {
   channelId: number;
@@ -19,8 +20,28 @@ export default function ChannelChatComponent(props: ChannelChatComponentProps) {
     const startConnection = async () => {
       await ChannelChatService.startConnection(props.channelId);
     };
-
+    
     startConnection();
+    //fetching the previous messages from the DB
+    wretch(`http://localhost:3001/api/chat/channel?channelId=${props.channelId}`)
+    .auth(`Bearer ${localStorage.getItem("jwt-token")}`)
+    .get()
+    .json((data) => {
+
+      const formattedMessages = data.messages.map((msg: any) => {
+        return {
+          senderId: msg.sender_id,
+          username: msg.senderUsername,
+          message: msg.message_content,
+          sentAt: msg.sent_at,
+        };
+      });
+  
+      setMessages(formattedMessages);
+      console.log("Formatted messages:", formattedMessages);  // Log to see the structure
+    })
+    .catch((err) => console.error(err));
+  
 
     const messageHandler = (
       senderId: number,
@@ -41,6 +62,7 @@ export default function ChannelChatComponent(props: ChannelChatComponentProps) {
 
   const sendMessage = () => {
     if (!message.trim()) return;
+    console.log(message);
     ChannelChatService.sendMessageToChannel(
       props.channelId,
       props.userId,
