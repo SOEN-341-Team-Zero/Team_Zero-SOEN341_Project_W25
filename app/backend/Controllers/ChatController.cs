bf0dc616cbc6e6e562cb4d05a25ca185fd0593ef
meloniouss <andrei.jianu31@gmail.com>
2025-03-19 23:18:05 -0400

    1liner

--- app/backend/Controllers/ChatController.cs
+++ app/backend/Controllers/ChatController.cs
@@ -132,7 +132,7 @@ public class ChatController : Controller
             var otherUserId = dm.user_id1 == userId ? dm.user_id2 : dm.user_id1;
             var otherUser = await _context.Users
                 .Where(u => u.user_id == otherUserId)
-                .Select(u => new { u.user_id, u.username, u.Activity) // bit of a hack})
+                .Select(u => new { u.user_id, u.username, u.Activity}) // bit of a hack})
                 .FirstOrDefaultAsync();
                 
             // Get messages for this DM
